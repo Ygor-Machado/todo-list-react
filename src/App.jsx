1c3f5bb839d3f1d836d3e551ebeda2894116c55c
Ygor-Machado <ygaounderground011@gmail.com>
2024-05-09 22:51:44 -0300

    inicia projeto

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </div>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
