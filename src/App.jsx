42f1898f27fa1f6f403bf8f0e9e79a1e8aa335e3
Ygor-Machado <ygaounderground011@gmail.com>
2024-05-11 20:15:57 -0300

    feat: salva no localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
@@ -8,88 +8,71 @@ import Filter from "./components/Filter";
 import "./App.css";
 
 function App() {
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("ASC");
+  const [search, setSearch] = useState("");
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
 
-  const [filter, setFilter] = useState("All")
-  const [sort, setSort] = useState("ASC")
-  const [search, setSearch] = useState("")
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "Rever a final do mundial 2012 corinthians x chelsea",
-      category: "Trabalho",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      text: "Ver o corinthians jogar",
-      category: "Pessoal",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      text: "Estudar React",
-      category: "Estudos",
-      isCompleted: false,
-    }
-  ]);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (text, category) => {
-
-    const newTodos = [...todos, {
+    const newTodo = {
       id: Math.floor(Math.random() * 1000),
       text,
       category,
       isCompleted: false
-    }]
+    };
 
-    setTodos(newTodos)
-  }
+    setTodos([...todos, newTodo]);
+  };
 
   const removeTodo = (id) => {
-    const newTodos = [...todos]
-    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null)
-
-    setTodos(filteredTodos)
-  }
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
 
   const completeTodo = (id) => {
-    const newTodos = [...todos]
-    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
-    setTodos(newTodos)
-  }
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodos(newTodos);
+  };
 
-  // const filteredTodos = todos.filter((todo) =>
-  //   filter === "All"
-  //     ? true
-  //     : filter === "Completed"
-  //     ? todo.isCompleted
-  //     : !todo.isCompleted
-  // );
-
-  // const sortedTodos = filteredTodos.sort((a, b) =>
-  //   sort === "ASC" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)
-  // );
-
-  // const searchedTodos = sortedTodos.filter((todo) =>
-  //   todo.text.toLowerCase().includes(search.toLowerCase())
-  // );
-  
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
-      <Search search={search} setSearch={setSearch}/>
-      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-         {/* {searchedTodos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
-        ))} */}
         {todos
-          .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-          .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
-          .sort((a, b) => sort === "ASC" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "ASC"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
           .map((todo) => (
-            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
-        ))}
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
